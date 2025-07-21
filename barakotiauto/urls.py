c397d5fc4197ff64d0b627b1019b9d669b97bd3c
Dipesh Barakoti <dbarakoti@gmail.com>
2025-07-21 10:09:27 -0600

    Added fix for url mapping and redirect

--- barakotiauto/urls.py
+++ barakotiauto/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from inventories.views import root_redirect_view
 
 
 urlpatterns = [
+    path('', root_redirect_view),
     path('admin/', admin.site.urls),
     path('api/', include('inventories.urls')),
 ]
--- inventories/views.py
+++ inventories/views.py
@@ -2,6 +2,11 @@
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from .models import Inventory
 from .serializers import InventorySerializer
+from django.shortcuts import redirect
+
+def root_redirect_view(request):
+    return redirect('/api/inventories/')
+
 
 
 class InventoryListView(ListAPIView):
